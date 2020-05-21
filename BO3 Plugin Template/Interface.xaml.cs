8ad0d3aa7e30fbcb15c6ac03dd541b319c17145f
Airyzz <airyzyt@gmail.com>
2020-05-21 16:24:16 +0930

    Initial Commit

---
+++ BO3 Plugin Template/Interface.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyBO3Plugin
+{
+    /// <summary>
+    /// Interaction logic for Interface.xaml
+    /// </summary>
+    public partial class Interface : UserControl
+    {
+        public Interface()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello, World!");
+        }
+    }
+}
---
+++ BO3 Plugin Template/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using AiryzPluginFramework;
+using System.Windows.Controls;
+
+namespace MyBO3Plugin
+{
+    public class Plugin : APlugin
+    {
+        public string TabName => "MY Plugin";
+        public string Name => "Test Plugin";
+        public string Author => "Author";
+        public string Description => "Template Plugin";
+        public string Version => "1.0.0";
+        public UserControl UI => ui;
+        public bool requiresUI => true;
+
+        private Interface ui = new Interface();
+
+        public int main()
+        {
+            return 1;
+        }
+
+        public bool ReadAllAddresses()
+        {
+            System.Console.WriteLine("Reading All Addresses for: " + Name + " Plugin");
+            return true;
+        }
+    }
+}
